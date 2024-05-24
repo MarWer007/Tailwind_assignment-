e39b238017de5ddcab07fd1a33eadebb5ae01a39
Jumanji <jumanji@debian-BULLSEYE-live-builder-AMD64>
2024-05-24 12:49:02 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans'],
+        alpha: ['Alpha Slab One'],
+      }
+    },
+  },
+  plugins: [],
+}
+
